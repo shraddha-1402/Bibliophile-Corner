e9135897a46cef6bb232d658756468711e45d214
shraddha-1402 <shraddha.gupta.1402@gmail.com>
2021-07-23 13:54:29 +0530

    functionality of the app implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import headerImg from "./undraw_Bibliophile_hwqc.svg";
+import { data } from "./data.js";
 
 function App() {
+  const [bookData, setBookData] = useState([]);
+
+  const clickHandler = (e) => {
+    setBookData([...data[e.target.innerText]]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav-bar">
+        <h1>Bibliophile's Corner</h1>
+      </nav>
+      <div className="container">
+        <img src={headerImg} className="header-image" alt="bibliophile"></img>
+        <div className="header-content">
+          <div className="search-options">
+            <button className="search-button" onClick={clickHandler}>
+              Rom-Com
+            </button>
+            <button className="search-button" onClick={clickHandler}>
+              Thriller
+            </button>
+            <button className="search-button" onClick={clickHandler}>
+              Fantasy
+            </button>
+          </div>
+          <div className="search-results">
+            {bookData.map((book, index) => {
+              return (
+                <div key={index} className="card">
+                  <img
+                    src={book.image}
+                    className="card-image"
+                    alt="book-cover"
+                  />
+                  <div className="card-detail">
+                    <h3 className="card-title">{book.name}</h3>
+                    <p className="card-paragraph">
+                      By: <strong>{book.author}</strong>
+                    </p>
+                    <p className="card-paragraph">{book.description}</p>
+                    {/* creates an array of book.rating times undefined*/}
+                    {[...Array(book.rating)].map((rate, index) => {
+                      return (
+                        <span
+                          key={index}
+                          className="card-rating"
+                          role="img"
+                          aria-label="star-rating"
+                        >
+                          ⭐
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,73 @@
+const data = {
+  "Rom-Com": [
+    {
+      name: "Book1",
+      author: "Jane austen",
+      description: "loream ipaskdfdsh",
+      image: "some url",
+      rating: 5
+    },
+    {
+      name: "Book1",
+      author: "Jane austen",
+      description: "loream ipaskdfdsh",
+      image: "some url",
+      rating: 5
+    },
+    {
+      name: "Book1",
+      author: "Jane austen",
+      description: "loream ipaskdfdsh",
+      image: "some url",
+      rating: 5
+    }
+  ],
+  Thriller: [
+    {
+      name: "Book1",
+      author: "Dan Brown",
+      description: "loream ipaskdfdsh",
+      image: "some url",
+      rating: 5
+    },
+    {
+      name: "Book1",
+      author: "Dan Brown",
+      description: "loream ipaskdfdsh",
+      image: "some url",
+      rating: 5
+    },
+    {
+      name: "Book1",
+      author: "Dan Brown",
+      description: "loream ipaskdfdsh",
+      image: "some url",
+      rating: 5
+    }
+  ],
+  Fantasy: [
+    {
+      name: "Book1",
+      author: "J K Rowling",
+      description: "loream ipaskdfdsh",
+      image: "some url",
+      rating: 5
+    },
+    {
+      name: "Book1",
+      author: "J K Rowling",
+      description: "loream ipaskdfdsh",
+      image: "some url",
+      rating: 5
+    },
+    {
+      name: "Book1",
+      author: "J K Rowling",
+      description: "loream ipaskdfdsh",
+      image: "some url",
+      rating: 5
+    }
+  ]
+};
+
+module.exports = {data};
