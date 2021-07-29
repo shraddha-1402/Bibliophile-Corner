092d14dc02df7944e0db998ced2121055bad33c4
shraddha-1402 <shraddha.gupta.1402@gmail.com>
2021-07-29 14:15:33 +0530

    added links of books on goodreads

--- src/App.js
+++ src/App.js
@@ -35,37 +35,39 @@ function App() {
           <div className="search-results">
             {bookData.map((book, index) => {
               return (
-                <div key={index} className="card">
-                  <div className="card-image-container">
-                    <img
-                      src={book.image}
-                      className="card-image"
-                      alt="book-cover"
-                    />
-                  </div>
-                  <div className="card-detail">
-                    <h3 className="card-title">{book.name}</h3>
-                    <p className="card-paragraph">
-                      By: <strong>{book.author}</strong>
-                    </p>
-                    <p className="card-paragraph">{book.description}</p>
-                    <div className="card-rating">
-                      {/* creates an array of book.rating times undefined*/}
-                      {[...Array(book.rating)].map((rate, index) => {
-                        return (
-                          <span
-                            key={index}
-                            className="card-rating"
-                            role="img"
-                            aria-label="star-rating"
-                          >
-                            ⭐
-                          </span>
-                        );
-                      })}
+                <a href={book.link} key={index} className="link" rel="noreferrer noopener" target="_blank">
+                  <div className="card">
+                    <div className="card-image-container">
+                      <img
+                        src={book.image}
+                        className="card-image"
+                        alt="book-cover"
+                      />
+                    </div>
+                    <div className="card-detail">
+                      <h3 className="card-title">{book.name}</h3>
+                      <p className="card-paragraph">
+                        By: <strong>{book.author}</strong>
+                      </p>
+                      <p className="card-paragraph">{book.description}</p>
+                      <div className="card-rating">
+                        {/* creates an array of book.rating times undefined*/}
+                        {[...Array(book.rating)].map((_rate, index) => {
+                          return (
+                            <span
+                              key={index}
+                              className="card-rating"
+                              role="img"
+                              aria-label="star-rating"
+                            >
+                              ⭐
+                            </span>
+                          );
+                        })}
+                      </div>
                     </div>
                   </div>
-                </div>
+                </a>
               );
             })}
           </div>
@@ -73,13 +75,13 @@ function App() {
       </div>
       <footer>
         <h3>Connect On</h3>
-        <a href="https://github.com/shraddha-1402" rel="noreferrer noopener" target="_blank" className="link">
+        <a href="https://github.com/shraddha-1402" rel="noreferrer noopener" target="_blank" className="link footer-link">
           <span className="fa fa-github" aria-hidden="true"></span>
         </a>
-        <a href="https://twitter.com/ShraddhaGupta08" rel="noreferrer noopener" target="_blank" className="link">
+        <a href="https://twitter.com/ShraddhaGupta08" rel="noreferrer noopener" target="_blank" className="link footer-link">
           <span className="fa fa-twitter" aria-hidden="true"></span>
         </a>
-        <a href="https://www.linkedin.com/in/shraddha-1402/" rel="noreferrer noopener" target="_blank" className="link">
+        <a href="https://www.linkedin.com/in/shraddha-1402/" rel="noreferrer noopener" target="_blank" className="link footer-link">
           <span className="fa fa-linkedin" aria-hidden="true"></span>
         </a>
       </footer>
